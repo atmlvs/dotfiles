58ef6597836d83fa03a04a7a6186decb6f7ee650
yingkzhe <yingkai.zheng@nokia.com>
2017-06-29 11:17:52 +0800

    merged with windows version

--- emacs.d/elisp/base-extensions.el
+++ emacs.d/elisp/base-extensions.el
@@ -132,8 +132,31 @@
                                "~/org-notes/todo.org")
         org-default-notes-file (concat org-directory "/todo.org")
 	org-plantuml-jar-path "~/.emacs.d/utils/plantuml.jar"
+	org-use-fast-todo-selection t ;C-c C-t KEY
+	org-treat-S-cursor-todo-selection-as-state-change nil
+	org-todo-keywords
+	(quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
+		(sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING")))
+	org-todo-keyword-faces
+	(quote (("TODO" :foreground "red" :weight bold)
+              ("NEXT" :foreground "blue" :weight bold)
+              ("DONE" :foreground "forest green" :weight bold)
+              ("WAITING" :foreground "orange" :weight (beginning-of-line)d)
+              ("HOLD" :foreground "magenta" :weight bold)
+              ("CANCELLED" :foreground "forest green" :weight bold)
+              ("MEETING" :foreground "forest green" :weight bold)
+              ("PHONE" :foreground "forest green" :weight bold)))
+	org-todo-state-tags-triggers
+	(quote (("CANCELLED" ("CANCELLED" . t))
+		("WAITING" ("WAITING" . t))
+		("HOLD" ("WAITING") ("HOLD" . t))
+		(done ("WAITING") ("HOLD"))
+		("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
+		("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
+		("DONE" ("WAITING") ("CANCELLED") ("HOLD"))))
         plantuml-mode-debug-enabled t
 	org-confirm-babel-evaluate nil)
+  (require 'ob-elixir)
   (require 'ob-python)
   (require 'ob-plantuml)
   (require 'ob-http)
@@ -313,8 +336,7 @@
 		  pyim-probe-punctuation-after-punctuation))
   :bind
   (("M-j" . pyim-convert-code-at-point)
-   ("C-;" . pyim-delete-word-from-personal-buffer))
-  )
+   ("C-;" . pyim-delete-word-from-personal-buffer)))
 
 (use-package org-download
   :config
--- emacs.d/elisp/base.el
+++ emacs.d/elisp/base.el
@@ -151,11 +151,26 @@
 
 (setq org-use-sub-superscripts nil)
 (setq org-export-with-sub-superscripts '{})
+
 ;; Set time mode
-(display-time-mode t)
+(display-time-mode 1)
 (setq display-time-24hr-format t)
 (setq display-time-day-and-date t)
 (setq display-time-interval 10)
+(defface egoge-display-time
+  '((((type x w32 mac))
+     ;; #060525 is the background colour of my default face.
+     (:foreground "#060525" :inherit bold))
+    (((type tty))
+     (:foreground "blue")))
+  "Face used to display the time in the mode line.")
+;; This causes the current time in the mode line to be displayed in
+;; `egoge-display-time-face' to make it stand out visually.
+(setq display-time-string-forms
+      '((propertize (concat " " 24-hours ":" minutes " ")
+		    'face 'egoge-display-time)))
+
+(setq tramp-default-method "ssh")
 
 (server-start)
 
