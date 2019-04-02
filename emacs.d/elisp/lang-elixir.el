04f56524d634fa87d014529eb0261dc4390964da
Zheng Yingkai <zhengyingkai@aerozhonghuan.com>
2019-04-02 22:17:43 +0800

    update vimrc and emacs elixir

--- emacs.d/elisp/lang-elixir.el
+++ emacs.d/elisp/lang-elixir.el
@@ -8,6 +8,8 @@
   :commands (flycheck-mix-setup))
 
 (use-package elixir-mode)
+(add-hook 'elixir-mode-hook
+	  (lambda () (add-hook 'before-save-hook 'elixir-format nil t)))
 (use-package erlang
   :bind (:map erlang-mode-map ("M-," . alchemist-goto-jump-back))
   :config
